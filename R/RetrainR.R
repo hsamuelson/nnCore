79a06b79e2c36aeb0b99163bc7e5f7bca7187cd1
hsamuelson <hsamuelson@eastsideprep.org>
2018-06-04 22:13:55 -0700

    Added RetrainR functionality. Transfering Work from sep repo to larger nnCore project

---
+++ R/RetrainR.R
@@ -0,0 +1,39 @@
+#
+# Henry Samuelson
+#
+# Algorithmn Retraining
+#
+# ** THIS IS UNFININSH AND STILL IN DEVELOPMENT **
+#
+
+
+# #Split Data set
+# tData <- infert[1:100,] #train data
+# test <- infert[101:length(infert[,1]),] #test data
+#
+# # Define net
+# infertNN2 <- nnCoreV2$new(education ~ ., data= tData, hidden = 6, plotData = T)
+#
+# #train net
+# infertNN2$train(4000, trace = 1e3, learn_rate = .0001)
+#
+#
+#
+#
+# #run tests
+# infertNN2$predict(data.matrix(cbind(1, test[,-1]))) == test[,1]
+
+
+#Higgs Dataset
+higgsDat <- read.csv("C:/Users/hsamuelson/Desktop/R/Higgs/training/training.csv")
+higgsDat <- higgsDat[,-1]
+
+trainData <- higgsDat[1:150,]
+testData <- higgsDat[151:230,]
+
+higgsNN2 <- nnCoreV2$new(Label ~ ., data= trainData, hidden = 30, plotData = T)
+higgsNN2$train(9999, trace = 1e3, learn_rate = .0001)
+
+
+score <- higgsNN2$predict(data.matrix(cbind(1, testData[,-32]))) == testData[,32]
+mean(score) #This will be the effective accuracy of the algorithm
