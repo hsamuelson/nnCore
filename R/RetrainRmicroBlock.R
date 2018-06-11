6d4cda2c0cd2b034b9bb5594b2251b92148b0442
hsamuelson <hsamuelson@users.noreply.github.com>
2018-06-11 13:28:45 -0700

    Fat Erroros found, cbind should ahve been rbind, was throwing no errors

--- R/RetrainRmicroBlock.R
+++ R/RetrainRmicroBlock.R
@@ -7,20 +7,21 @@
 #
 
 #For desktop training file is located on desktop folder
-
+#higgsDat <- read.csv("C:/Users/hsamuelson/Desktop/training.csv")
 testData <- higgsDat[151:330,]
 
 #Higgs Dataset
 higgsDat <- read.csv("C:/Users/hsamuelson/Desktop/R/Higgs/training/training.csv")
 higgsDat <- higgsDat[,-1]
 for(i in 1:20){
+  i = 1
   trainData <- higgsDat[1:150,]
   testData <- higgsDat[151+(i*151):330+(i*151),]
-  refernceData <- higgsDat[500:700,]
+  refernceData <- higgsDat[249800:250000,]
 
 
   higgsNN2 <- nnCoreV3$new(Label ~ ., data= trainData, hidden = 30, plotData = T)
-  higgsNN2$train(9999, trace = 1e3, learn_rate = .0001)
+  higgsNN2$train(7999, trace = 1e3, learn_rate = .0001)
   #
   # By training will add on to var superScore but will be overwritten when an $predict() command is run.
   #
@@ -36,6 +37,7 @@ for(i in 1:20){
   #~.61666
   #~.61111
   print(refScore)
+
 
 
   #higgsNN2$predict(data.matrix(cbind(1, testData[1:2,-32])))
@@ -59,14 +61,14 @@ for(i in 1:20){
   #
   # *THIS IS WHERE THIS IS DIFFERENT *
   # unlike retrainR instead of adding all the new truth we need to test every indivudal part.
-  passingTruth <- numeric(length(newTruth[1,]))
+  passingTruth <- list()
 
   for(j in 1:length(newTruth[,1])) {# for every item compute new accuracy
-    tempTrain <- cbind(trainData, newTruth[j,])
+    tempTrain <- rbind(trainData, newTruth[j,])
     higgsNN3 <- nnCoreV3$new(Label ~ ., data= trainData, hidden = 30, plotData = T)
     cat("DataTest Round:", j)
 
-    higgsNN3$train(9999, trace = 1e3, learn_rate = .0001)
+    higgsNN3$train(7999, trace = 1e3, learn_rate = .0001)
     refScorez <- mean(higgsNN3$predict(data.matrix(cbind(1, refernceData[,-32]))) == refernceData[,32])
     print(refScorez)
     if((refScorez - refScore) > 0.01){
@@ -75,5 +77,5 @@ for(i in 1:20){
     }
   }
   #ammend newTruth to the trainingset
-  trainData <- rbind(trainData, newTruth)
+  #trainData <- rbind(trainData, newTruth)
 }
